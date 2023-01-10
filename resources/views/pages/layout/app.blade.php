d578fee917a2c61f74a22697eaed73ea7af570a5
Gnosis <gnosis@Gnosis-MacBook-Pro.local>
2023-01-10 16:26:00 +0100

    commit changes

--- resources/views/pages/layout/app.blade.php
+++ resources/views/pages/layout/app.blade.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="../images/favicon.ico">
 
     <title>Crypto Tokenizer UI Interface & Cryptocurrency Admin Template</title>
 
@@ -16,6 +15,15 @@
     <!-- Style-->
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('css/skin_color.css') }}">
+
+    <script src="https://cdn.ethers.io/lib/ethers-5.2.umd.min.js"
+            type="application/javascript"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" crossorigin="anonymous"></script>
+    <script src="{{ asset('chain-common.js') }}"></script>
+    <script src="{{ asset('polyfills.js') }}"></script>
+    <script src="{{ asset('chain-bundle.js') }}"></script>
+    <script type="text/javascript" src="https://unpkg.com/@walletconnect/web3-provider@1.2.1/dist/umd/index.min.js"></script>
+    <script type="text/javascript" src="https://unpkg.com/web3modal@1.9.0/dist/index.js"></script>
 
 </head>
 
@@ -57,7 +65,7 @@
 
             <div class="navbar-custom-menu r-side">
                 <ul class="nav navbar-nav">
-                    <a href="" class="btn btn-primary mr-3"> Connect Wallet</a>
+                    <a  id="metamaskbutton" class="btn btn-primary mr-3"> Connect Wallet</a>
 
 
                 </ul>
@@ -89,13 +97,13 @@
                         </li>
                         <li class="header">Apps &amp; UI </li>
 
-                        <li class="treeview">
+                        <li class="">
                             <a href="#">
                                 <img src="https://crypto-tokenizer-admin-dashboard.multipurposethemes.com/bs4/images/svg-icon/sidebar-menu/apps.svg" class="svg-icon" alt="">
                                 <span>Apps</span>
                                 <span class="pull-right-container">
-					  <i class="fa fa-angle-right pull-right"></i>
-					</span>
+                                  <i class="fa fa-angle-right pull-right"></i>
+                                </span>
                             </a>
                             <ul class="treeview-menu">
                                 <li class="treeview">
@@ -192,142 +200,6 @@
                                 </li>
                             </ul>
                         </li>
-                        <li class="treeview">
-                            <a href="#">
-                                <img src="https://crypto-tokenizer-admin-dashboard.multipurposethemes.com/bs4/images/svg-icon/sidebar-menu/uielements.svg" class="svg-icon" alt="">
-                                <span>UI Elements</span>
-                                <span class="pull-right-container">
-					  <i class="fa fa-angle-right pull-right"></i>
-					</span>
-                            </a>
-                            <ul class="treeview-menu">
-                                <li class="treeview">
-                                    <a href="#">
-                                        <i class="ti-more"></i>
-                                        <span>General UI</span>
-                                        <span class="pull-right-container">
-						  <i class="fa fa-angle-right pull-right"></i>
-						</span>
-                                    </a>
-                                    <ul class="treeview-menu">
-                                        <li class="treeview">
-                                            <a href="#">
-                                                <i class="ti-more"></i>
-                                                <span>User Interface</span>
-                                                <span class="pull-right-container">
-							  <i class="fa fa-angle-right pull-right"></i>
-							</span>
-                                            </a>
-                                            <ul class="treeview-menu">
-                                                <li><a href="ui_typography.html"><i class="ti-more"></i>Typography</a></li>
-                                                <li><a href="ui_grid.html"><i class="ti-more"></i>Grid</a></li>
-                                                <li><a href="ui_tab.html"><i class="ti-more"></i>Tabs</a></li>
-                                                <li><a href="ui_timeline.html"><i class="ti-more"></i>Timeline</a></li>
-                                                <li><a href="ui_timeline_horizontal.html"><i class="ti-more"></i>Horizontal</a></li>
-                                            </ul>
-                                        </li>
-                                        <li class="treeview">
-                                            <a href="#">
-                                                <i class="ti-more"></i>
-                                                <span>Component</span>
-                                                <span class="pull-right-container">
-							  <i class="fa fa-angle-right pull-right"></i>
-							</span>
-                                            </a>
-                                            <ul class="treeview-menu">
-                                                <li><a href="component_bootstrap_switch.html"><i class="ti-more"></i>Bootstrap Switch</a></li>
-                                                <li><a href="component_date_paginator.html"><i class="ti-more"></i>Date Paginator</a></li>
-                                                <li><a href="component_media_advanced.html"><i class="ti-more"></i>Advanced Medias</a></li>
-                                                <li><a href="component_modals.html"><i class="ti-more"></i>Modals</a></li>
-                                                <li><a href="component_rangeslider.html"><i class="ti-more"></i>Range Slider</a></li>
-                                                <li><a href="component_rating.html"><i class="ti-more"></i>Ratings</a></li>
-                                                <li><a href="component_animations.html"><i class="ti-more"></i>Animations</a></li>
-                                            </ul>
-                                        </li>
-                                        <li class="treeview">
-                                            <a href="#">
-                                                <i class="ti-more"></i>
-                                                <span>Cards</span>
-                                                <span class="pull-right-container">
-							  <i class="fa fa-angle-right pull-right"></i>
-							</span>
-                                            </a>
-                                            <ul class="treeview-menu">
-                                                <li><a href="box_advanced.html"><i class="ti-more"></i>Advanced Card</a></li>
-                                                <li><a href="box_basic.html"><i class="ti-more"></i>Basic Card</a></li>
-                                                <li><a href="box_color.html"><i class="ti-more"></i>Card Color</a></li>
-                                            </ul>
-                                        </li>
-                                        <li>
-                                            <a href="ui_color_utilities.html">
-                                                <i class="ti-more"></i>
-                                                <span>Color</span>
-                                            </a>
-                                        </li>
-                                        <li class="treeview">
-                                            <a href="#">
-                                                <i class="ti-more"></i>
-                                                <span>Icons</span>
-                                                <span class="pull-right-container">
-							  <i class="fa fa-angle-right pull-right"></i>
-							</span>
-                                            </a>
-                                            <ul class="treeview-menu">
-                                                <li><a href="icons_fontawesome.html"><i class="ti-more"></i>Font Awesome</a></li>
-                                                <li><a href="icons_glyphicons.html"><i class="ti-more"></i>Glyphicons</a></li>
-                                                <li><a href="icons_material.html"><i class="ti-more"></i>Material Icons</a></li>
-                                                <li><a href="icons_themify.html"><i class="ti-more"></i>Themify Icons</a></li>
-                                                <li><a href="icons_simpleline.html"><i class="ti-more"></i>Simple Line Icons</a></li>
-                                                <li><a href="icons_cryptocoins.html"><i class="ti-more"></i>Cryptocoins Icons</a></li>
-                                                <li><a href="icons_flag.html"><i class="ti-more"></i>Flag Icons</a></li>
-                                                <li><a href="icons_weather.html"><i class="ti-more"></i>Weather Icons</a></li>
-                                            </ul>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li class="treeview">
-                                    <a href="#">
-                                        <i class="ti-more"></i>
-                                        <span>Advanced UI</span>
-                                        <span class="pull-right-container">
-						  <i class="fa fa-angle-right pull-right"></i>
-						</span>
-                                    </a>
-                                    <ul class="treeview-menu">
-                                        <li><a href="ui_badges.html"><i class="ti-more"></i>Badges</a></li>
-                                        <li><a href="ui_border_utilities.html"><i class="ti-more"></i>Border</a></li>
-                                        <li><a href="ui_buttons.html"><i class="ti-more"></i>Buttons</a></li>
-                                        <li><a href="ui_dropdown.html"><i class="ti-more"></i>Dropdown</a></li>
-                                        <li><a href="ui_dropdown_grid.html"><i class="ti-more"></i>Dropdown Grid</a></li>
-                                        <li><a href="ui_progress_bars.html"><i class="ti-more"></i>Progress Bars</a></li>
-                                        <li><a href="ui_ribbons.html"><i class="ti-more"></i>Ribbons</a></li>
-                                        <li><a href="ui_sliders.html"><i class="ti-more"></i>Sliders</a></li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </li>
-                        <li class="treeview">
-                            <a href="#">
-                                <img src="https://crypto-tokenizer-admin-dashboard.multipurposethemes.com/bs4/images/svg-icon/sidebar-menu/forms1.svg" class="svg-icon" alt="">
-                                <span>Forms Elements</span>
-                                <span class="pull-right-container">
-					  <i class="fa fa-angle-right pull-right"></i>
-					</span>
-                            </a>
-                            <ul class="treeview-menu">
-                                <li><a href="forms_advanced.html"><i class="ti-more"></i>Form Elements</a></li>
-                                <li><a href="forms_mask.html"><i class="ti-more"></i>Formatter</a></li>
-                                <li><a href="forms_xeditable.html"><i class="ti-more"></i>Xeditable Editor</a></li>
-                                <li><a href="forms_code_editor.html"><i class="ti-more"></i>Code Editor</a></li>
-                                <li><a href="forms_editors.html"><i class="ti-more"></i>Editor</a></li>
-                                <li><a href="forms_dropzone.html"><i class="ti-more"></i>Dropzone</a></li>
-                                <li><a href="forms_editor_markdown.html"><i class="ti-more"></i>Markdown</a></li>
-                                <li><a href="forms_general.html"><i class="ti-more"></i>Layout</a></li>
-                                <li><a href="forms_wizard.html"><i class="ti-more"></i>Wizard</a></li>
-                                <li><a href="forms_validation.html"><i class="ti-more"></i>Validation</a></li>
-                            </ul>
-                        </li>
-
 
                     </ul>
                     <div class="ps__rail-x" style="left: 0px; bottom: 0px;"><div class="ps__thumb-x" tabindex="0" style="left: 0px; width: 0px;"></div></div><div class="ps__rail-y" style="top: 0px; height: 578px; right: 0px;"><div class="ps__thumb-y" tabindex="0" style="top: 0px; height: 383px;"></div></div></div>
