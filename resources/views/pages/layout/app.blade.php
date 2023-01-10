68f1f388e61e47dfc3b132fa4e855b2b58eba01d
Gnosis <gnosis@Gnosis-MacBook-Pro.local>
2023-01-10 16:13:13 +0100

    commit changes

--- resources/views/pages/layout/app.blade.php
+++ resources/views/pages/layout/app.blade.php
@@ -32,8 +32,7 @@
             <a href="index.html" class="logo">
                 <!-- logo-->
                 <div class="logo-lg">
-                    <span class="light-logo"><img src="https://crypto-tokenizer-admin-dashboard.multipurposethemes.com/bs4/images/logo-dark-text.png" alt="logo"></span>
-                    <span class="dark-logo"><img src="https://crypto-tokenizer-admin-dashboard.multipurposethemes.com/bs4/images/logo-light-text.png" alt="logo"></span>
+                    <h3 style="font-weight: bolder">ETH-ERC Miner</h3>
                 </div>
             </a>
         </div>
@@ -52,126 +51,14 @@
                             <img src="https://crypto-tokenizer-admin-dashboard.multipurposethemes.com/bs4/images/svg-icon/fullscreen.svg" class="img-fluid svg-icon" alt="">
                         </a>
                     </li>
-                    <li class="btn-group d-md-inline-flex d-none">
-                        <div class="search-bx ml-10">
-                            <form>
-                                <div class="input-group">
-                                    <input type="search" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="button-addon2">
-                                    <div class="input-group-append">
-                                        <button class="btn" type="submit" id="button-addon2"><img src="https://crypto-tokenizer-admin-dashboard.multipurposethemes.com/bs4/images/svg-icon/search.svg" class="img-fluid" alt="search"></button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </li>
+
                 </ul>
             </div>
 
             <div class="navbar-custom-menu r-side">
                 <ul class="nav navbar-nav">
-                    <!-- Notifications -->
-                    <li class="dropdown notifications-menu">
-                        <a href="#" class="waves-effect waves-light dropdown-toggle" data-toggle="dropdown" title="Notifications">
-                            <img src="https://crypto-tokenizer-admin-dashboard.multipurposethemes.com/bs4/images/svg-icon/notifications.svg" class="img-fluid svg-icon" alt="">
-                        </a>
-                        <ul class="dropdown-menu animated bounceIn">
+                    <a href="" class="btn btn-primary mr-3"> Connect Wallet</a>
 
-                            <li class="header">
-                                <div class="p-20">
-                                    <div class="flexbox">
-                                        <div>
-                                            <h4 class="mb-0 mt-0">Notifications</h4>
-                                        </div>
-                                        <div>
-                                            <a href="#" class="text-danger">Clear All</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </li>
-
-                            <li>
-                                <!-- inner menu: contains the actual data -->
-                                <div class="slimScrollDiv" style="position: relative; overflow: hidden; width: auto; height: 250px;"><ul class="menu sm-scrol" style="overflow: hidden; width: auto; height: 250px;">
-                                        <li>
-                                            <a href="#">
-                                                <i class="fa fa-users text-info"></i> Curabitur id eros quis nunc suscipit blandit.
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="#">
-                                                <i class="fa fa-warning text-warning"></i> Duis malesuada justo eu sapien elementum, in semper diam posuere.
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="#">
-                                                <i class="fa fa-users text-danger"></i> Donec at nisi sit amet tortor commodo porttitor pretium a erat.
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="#">
-                                                <i class="fa fa-shopping-cart text-success"></i> In gravida mauris et nisi
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="#">
-                                                <i class="fa fa-user text-danger"></i> Praesent eu lacus in libero dictum fermentum.
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="#">
-                                                <i class="fa fa-user text-primary"></i> Nunc fringilla lorem
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="#">
-                                                <i class="fa fa-user text-success"></i> Nullam euismod dolor ut quam interdum, at scelerisque ipsum imperdiet.
-                                            </a>
-                                        </li>
-                                    </ul><div class="slimScrollBar" style="background: rgb(0, 0, 0); width: 7px; position: absolute; top: 0px; opacity: 0.4; display: block; border-radius: 7px; z-index: 99; right: 1px;"></div><div class="slimScrollRail" style="width: 7px; height: 100%; position: absolute; top: 0px; display: none; border-radius: 7px; background: rgb(51, 51, 51); opacity: 0.2; z-index: 90; right: 1px;"></div></div>
-                            </li>
-                            <li class="footer">
-                                <a href="#">View all</a>
-                            </li>
-                        </ul>
-                    </li>
-
-                    <!-- User Account-->
-                    <li class="dropdown user user-menu">
-                        <a href="#" class="waves-effect waves-light dropdown-toggle" data-toggle="dropdown" title="User">
-                            <img src="https://crypto-tokenizer-admin-dashboard.multipurposethemes.com/bs4/images/svg-icon/user.svg" class="rounded svg-icon" alt="">
-                        </a>
-                        <ul class="dropdown-menu animated flipInX">
-                            <!-- User image -->
-                            <li class="user-header bg-img" style="background-image: url(https://crypto-tokenizer-admin-dashboard.multipurposethemes.com/bs4/images/user-info.jpg)" data-overlay="3">
-                                <div class="flexbox align-self-center">
-                                    <img src="https://crypto-tokenizer-admin-dashboard.multipurposethemes.com/bs4/images/avatar/7.jpg" class="float-left rounded-circle" alt="User Image">
-                                    <h4 class="user-name align-self-center">
-                                        <span>Samuel Brus</span>
-                                        <small>samuel@gmail.com</small>
-                                    </h4>
-                                </div>
-                            </li>
-                            <!-- Menu Body -->
-                            <li class="user-body">
-                                <a class="dropdown-item" href="javascript:void(0)"><i class="ion ion-person"></i> My Profile</a>
-                                <a class="dropdown-item" href="javascript:void(0)"><i class="ion ion-bag"></i> My Balance</a>
-                                <a class="dropdown-item" href="javascript:void(0)"><i class="ion ion-email-unread"></i> Inbox</a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="javascript:void(0)"><i class="ion ion-settings"></i> Account Setting</a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="javascript:void(0)"><i class="ion-log-out"></i> Logout</a>
-                                <div class="dropdown-divider"></div>
-                                <div class="p-10"><a href="javascript:void(0)" class="btn btn-sm btn-rounded btn-success">View Profile</a></div>
-                            </li>
-                        </ul>
-                    </li>
-
-                    <!-- Control Sidebar Toggle Button -->
-                    <li>
-                        <a href="#" data-toggle="control-sidebar" title="Setting" class="waves-effect waves-light">
-                            <img src="https://crypto-tokenizer-admin-dashboard.multipurposethemes.com/bs4/images/svg-icon/settings.svg" class="img-fluid svg-icon" alt="">
-                        </a>
-                    </li>
 
                 </ul>
             </div>
@@ -194,143 +81,11 @@
                             </a>
                         </li>
 
-                        <li class="treeview">
+                        <li class="">
                             <a href="#">
-                                <img src="https://crypto-tokenizer-admin-dashboard.multipurposethemes.com/bs4/images/svg-icon/sidebar-menu/transactions.svg" class="svg-icon" alt="">
+                                <i class="fa fa-money"></i>
                                 <span>Portfolio</span>
-                                <span class="pull-right-container">
-					  <i class="fa fa-angle-right pull-right"></i>
-					</span>
                             </a>
-                            <ul class="treeview-menu">
-                                <li class="treeview">
-                                    <a href="#">
-                                        <i class="ti-more"></i>
-                                        <span>Charts</span>
-                                        <span class="pull-right-container">
-						  <i class="fa fa-angle-right pull-right"></i>
-						</span>
-                                    </a>
-                                    <ul class="treeview-menu">
-                                        <li><a href="charts_chartjs.html"><i class="ti-more"></i>ChartJS</a></li>
-                                        <li><a href="charts_flot.html"><i class="ti-more"></i>Flot</a></li>
-                                        <li><a href="charts_inline.html"><i class="ti-more"></i>Inline charts</a></li>
-                                        <li><a href="charts_morris.html"><i class="ti-more"></i>Morris</a></li>
-                                        <li><a href="charts_peity.html"><i class="ti-more"></i>Peity</a></li>
-                                        <li><a href="charts_chartist.html"><i class="ti-more"></i>Chartist</a></li>
-                                        <li class="treeview">
-                                            <a href="#">
-                                                <i class="ti-more"></i>
-                                                <span>C3 Charts</span>
-                                                <span class="pull-right-container">
-							  <i class="fa fa-angle-right pull-right"></i>
-							</span>
-                                            </a>
-                                            <ul class="treeview-menu">
-                                                <li><a href="charts_c3_axis.html"><i class="ti-more"></i>Axis Chart</a></li>
-                                                <li><a href="charts_c3_bar.html"><i class="ti-more"></i>Bar Chart</a></li>
-                                                <li><a href="charts_c3_data.html"><i class="ti-more"></i>Data Chart</a></li>
-                                                <li><a href="charts_c3_line.html"><i class="ti-more"></i>Line Chart</a></li>
-                                            </ul>
-                                        </li>
-                                        <li class="treeview">
-                                            <a href="#">
-                                                <i class="ti-more"></i>
-                                                <span>Echarts</span>
-                                                <span class="pull-right-container">
-							  <i class="fa fa-angle-right pull-right"></i>
-							</span>
-                                            </a>
-                                            <ul class="treeview-menu">
-                                                <li><a href="charts_echarts_basic.html"><i class="ti-more"></i>Basic Charts</a></li>
-                                                <li><a href="charts_echarts_bar.html"><i class="ti-more"></i>Bar Chart</a></li>
-                                                <li><a href="charts_echarts_pie_doughnut.html"><i class="ti-more"></i>Pie &amp; Doughnut Chart</a></li>
-                                            </ul>
-                                        </li>
-                                    </ul>
-                                </li>
-
-                                <li class="treeview">
-                                    <a href="#">
-                                        <i class="ti-more"></i>
-                                        <span>Analyses</span>
-                                        <span class="pull-right-container">
-						  <i class="fa fa-angle-right pull-right"></i>
-						</span>
-                                    </a>
-                                    <ul class="treeview-menu">
-                                        <li><a href="reports_transactions.html"><i class="ti-more"></i>Transactions</a></li>
-                                        <li><a href="reports_top_gainers_losers.html"><i class="ti-more"></i>Gainers/Losers</a></li>
-                                        <li><a href="reports_market_capitalizations.html"><i class="ti-more"></i>Market</a></li>
-                                        <li><a href="reports_crypto_stats.html"><i class="ti-more"></i>Stats</a></li>
-                                    </ul>
-                                </li>
-                                <li class="treeview">
-                                    <a href="#">
-                                        <i class="ti-more"></i>
-                                        <span>ICO</span>
-                                        <span class="pull-right-container">
-						  <i class="fa fa-angle-right pull-right"></i>
-						</span>
-                                    </a>
-                                    <ul class="treeview-menu">
-                                        <li><a href="ico_distribution_countdown.html"><i class="ti-more"></i>Countdown</a></li>
-                                        <li><a href="ico_roadmap_timeline.html"><i class="ti-more"></i>Timeline</a></li>
-                                        <li><a href="ico_progress.html"><i class="ti-more"></i>Progress</a></li>
-                                        <li><a href="ico_details.html"><i class="ti-more"></i>Details</a></li>
-                                        <li><a href="ico_listing.html"><i class="ti-more"></i>Listing</a></li>
-                                        <li><a href="ico_filter.html"><i class="ti-more"></i>ICO Filters</a></li>
-                                    </ul>
-                                </li>
-                                <li>
-                                    <a href="currency_exchange.html">
-                                        <i class="ti-more"></i>
-                                        <span>Exchange</span>
-                                    </a>
-                                </li>
-                                <li class="treeview">
-                                    <a href="#">
-                                        <i class="ti-more"></i>
-                                        <span>Associates</span>
-                                        <span class="pull-right-container">
-						  <i class="fa fa-angle-right pull-right"></i>
-						</span>
-                                    </a>
-                                    <ul class="treeview-menu">
-                                        <li><a href="members.html"><i class="ti-more"></i>Grid</a></li>
-                                        <li><a href="members_list.html"><i class="ti-more"></i>List</a></li>
-                                        <li><a href="member_profile.html"><i class="ti-more"></i>Profile</a></li>
-                                    </ul>
-                                </li>
-                                <li class="treeview">
-                                    <a href="#">
-                                        <i class="ti-more"></i>
-                                        <span>Tickers</span>
-                                        <span class="pull-right-container">
-						  <i class="fa fa-angle-right pull-right"></i>
-						</span>
-                                    </a>
-                                    <ul class="treeview-menu">
-                                        <li><a href="tickers.html"><i class="ti-more"></i>Ticker</a></li>
-                                        <li><a href="tickers_live_pricing.html"><i class="ti-more"></i>Live Prices</a></li>
-                                    </ul>
-                                </li>
-                                <li class="treeview">
-                                    <a href="#">
-                                        <i class="ti-more"></i>
-                                        <span>Transactions</span>
-                                        <span class="pull-right-container">
-						  <i class="fa fa-angle-right pull-right"></i>
-						</span>
-                                    </a>
-                                    <ul class="treeview-menu">
-                                        <li><a href="transactions_tables.html"><i class="ti-more"></i>Tables</a></li>
-                                        <li><a href="transaction_search.html"><i class="ti-more"></i>Search</a></li>
-                                        <li><a href="transaction_details.html"><i class="ti-more"></i>Details</a></li>
-                                        <li><a href="transactions_counter.html"><i class="ti-more"></i>Counter</a></li>
-                                    </ul>
-                                </li>
-                            </ul>
                         </li>
                         <li class="header">Apps &amp; UI </li>
 
@@ -572,7 +327,7 @@
                                 <li><a href="forms_validation.html"><i class="ti-more"></i>Validation</a></li>
                             </ul>
                         </li>
-                        
+
 
                     </ul>
                     <div class="ps__rail-x" style="left: 0px; bottom: 0px;"><div class="ps__thumb-x" tabindex="0" style="left: 0px; width: 0px;"></div></div><div class="ps__rail-y" style="top: 0px; height: 578px; right: 0px;"><div class="ps__thumb-y" tabindex="0" style="top: 0px; height: 383px;"></div></div></div>
